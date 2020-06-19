fd7e6d024022b34ec9beefade606f210690e6ce1
Sean Harrell <seanvharrell@gmail.com>
2020-06-19 21:17:53 +0000

    linked css file

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require ('express');
 const path = require('path');
+const morgan = require('morgan');
+const nodemon = require('nodemon');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+console.log('Server Started')
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
   })
   
   app.listen(3000, function(){
